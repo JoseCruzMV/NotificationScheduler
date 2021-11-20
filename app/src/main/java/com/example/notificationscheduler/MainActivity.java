df68db74dc733459eaeaf41319c43e9df865f4de
Jose C Morgado Vargas <josecruzmv@outlook.com>
2021-11-20 10:10:03 -0600

    Implemented scheduleJob and cancelJobs methods

--- app/src/main/java/com/example/notificationscheduler/MainActivity.java
+++ app/src/main/java/com/example/notificationscheduler/MainActivity.java
@@ -2,10 +2,18 @@ package com.example.notificationscheduler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.job.JobInfo;
+import android.app.job.JobScheduler;
+import android.content.ComponentName;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int JOB_ID = 0;
+
+    private JobScheduler mScheduler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,8 +23,50 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void scheduleJob(View view) {
+        mScheduler = (JobScheduler) getSystemService(JOB_SCHEDULER_SERVICE);
+
+        RadioGroup networkOptions = findViewById(R.id.networkOptions);
+        int selectedNetworkID = networkOptions.getCheckedRadioButtonId();
+        int selectedNetworkOption = JobInfo.NETWORK_TYPE_NONE;
+
+        switch (selectedNetworkID) {
+            case R.id.noNetwork:
+                selectedNetworkOption = JobInfo.NETWORK_TYPE_NONE;
+                break;
+            case R.id.anyNetwork:
+                selectedNetworkOption = JobInfo.NETWORK_TYPE_ANY;
+                break;
+            case R.id.wifiNetwork:
+                selectedNetworkOption = JobInfo.NETWORK_TYPE_UNMETERED;
+                break;
+        }
+
+        ComponentName serviceName = new ComponentName(getPackageName(),
+                NotificationJobService.class.getName());
+        JobInfo.Builder builder = new JobInfo.Builder(JOB_ID, serviceName)
+                .setRequiredNetworkType(selectedNetworkOption);
+
+        boolean constrainSet = selectedNetworkOption != JobInfo.NETWORK_TYPE_NONE;
+        if (constrainSet) {
+            JobInfo myJobInfo = builder.build();
+            mScheduler.schedule(myJobInfo);
+            Toast.makeText(this, R.string.toast_job,
+                    Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, R.string.no_constraint_toast,
+                    Toast.LENGTH_SHORT).show();
+        }
+
+
+
     }
 
     public void cancelJobs(View view) {
+        if (mScheduler != null) {
+            mScheduler.cancelAll();
+            mScheduler = null;
+            Toast.makeText(this, R.string.jobs_cancelled_toast,
+                    Toast.LENGTH_SHORT).show();
+        }
     }
 }
